2e204016c68d8eb487776b1c4bc55010f13ba2a8
Imran Hussain <fswebenthusiast@gmail.com>
2024-05-09 20:21:04 +0500

    feat: add route '/info'

--- index.js
+++ index.js
@@ -2,35 +2,46 @@ const express = require("express")
 
 const app = express()
 
+const data = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
 app.get("/api/persons", (req, res)=>{
 
-    const data = [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-    ]
 
     res.json(data)
 })
 
-const port = 3000;
+app.get("/info", (req, res)=>{
+  const persons = data.length;
+const content = `<p>Phonebook has info for ${persons} people</p>`
+const date = new Date(); 
+console.log("🚀 ~ app.get ~ date:", date)
+
+const currentDate = `<p>${date}</p>`
+  res.send(`${content} ${currentDate}`)
+})
+
+const port = 3001;
 
 
 app.listen(port, ()=>{
