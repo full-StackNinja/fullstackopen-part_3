36a1246c887c1258517c859d44484e51bec3e5a1
Imran Hussain <fswebenthusiast@gmail.com>
2024-05-18 09:50:08 +0500

    refact: change data fetching and storage to mongodb

--- index.js
+++ index.js
@@ -1,116 +1,129 @@
-const express = require("express")
-const morgan = require("morgan")
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+const Person = require("./Person");
+const { default: mongoose } = require("mongoose");
 
-require("dotenv").config()
+require("dotenv").config();
 
-let data = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+mongoose
+   .connect(process.env.MONGO_URI)
+   .then(() => {
+      console.log("database connected!");
+   })
+   .catch((err) => {
+      throw new Error(err.message);
+   });
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-morgan.token('req-body', function(req){return JSON.stringify(req.body)})
+morgan.token("req-body", function (req) {
+   return JSON.stringify(req.body);
+});
 
+app.use(
+   morgan(
+      ":method :url :status :res[content-length] - :response-time ms :req-body",
+   ),
+);
+// Load frontend HTML on start
+app.use(express.static("dist"));
 
+app.get("/info", (req, res, next) => {
+   Person.countDocuments({})
+      .then((count) => {
+         const content = `<p>Phonebook has info for ${count} people</p>`;
+         const date = new Date();
+         const currentDate = `<p>${date}</p>`;
+         res.send(`${content} ${currentDate}`);
+      })
+      .catch((err) => {
+         next(err);
+      });
+});
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
+app.get("/api/persons", async (req, res) => {
+   const data = await Person.find({});
+   res.json(data);
+});
 
-app.use(express.static('dist'))
+app.post("/api/persons", async (req, res) => {
+   const { name, number } = req.body;
+   if (!name) return res.status(422).send("name is required");
+   if (!number) return res.status(422).send("number is required");
+   const nameExist = await Person.findOne({ name: name });
+   if (nameExist) return res.status(422).send("name must be unique");
+   const newPerson = new Person({
+      name,
+      number,
+   });
+   await newPerson.save().catch((err) => {
+      throw new Error(err.message);
+   });
+   res.status(201).json(newPerson);
+});
 
-app.get("/info", (req, res)=>{
-  const persons = data.length;
-const content = `<p>Phonebook has info for ${persons} people</p>`
-const date = new Date(); 
-console.log("🚀 ~ app.get ~ date:", date)
+app.get("/api/persons/:id", async (req, res) => {
+   const id = req.params.id;
+   const person = await Person.findById(id);
 
-const currentDate = `<p>${date}</p>`
-  res.send(`${content} ${currentDate}`)
-})
+   if (person) {
+      res.json(person);
+      return;
+   }
+   res.status(404).send(`Person with id ${id} not found`);
+});
 
-app.get("/api/persons", (req, res)=>{
+app.put("/api/persons/:id", async (req, res, next) => {
+   try {
+      const { id } = req.params;
+      const { name, number } = req.body;
+      const person = { _id: id, name, number };
+      const updatedPerson = await Person.findByIdAndUpdate(id, person, {
+         new: true,
+      });
+      if (!updatedPerson)
+         return res
+            .status(404)
+            .send(`Person with id ${id} has already been deleted`);
+      res.json(updatedPerson);
+   } catch (err) {
+      next(err);
+   }
+});
 
+app.delete("/api/persons/:id", async (req, res, next) => {
+   try {
+      const id = req.params.id;
+      const person = await Person.findById(id);
+      if (!person)
+         return res.status(404).send(`Person with id ${id} not found`);
+      const deletedPerson = await Person.findOneAndDelete({ _id: id });
+      res.status(200).json(deletedPerson);
+   } catch (err) {
+      next(err);
+   }
+});
 
-    res.json(data)
-})
+// Unknown endpoint error
+app.use((req, res, next) => {
+   res.status(404).json({ err: "Unknown endpoint!" });
+});
 
-app.post("/api/persons", (req, res)=>{
-  const {name, number} = req.body
-  console.log("🚀 ~ app.post ~ number:", number)
-  console.log("🚀 ~ app.post ~ name:", name)
-  
-  if(!name) return res.status(422).send("name is required")
-  if(!number) return res.status(422).send("number is required")
-  const nameExist = data.find(person=>person.name === name)
-if(nameExist) return res.status(422).send("name must be unique")
-  const id = Number.parseInt(Math.random()*Number.MAX_SAFE_INTEGER, 10);
-  const newPerson = {
-    id, name, number
-  }
-  data.push(newPerson)
-  res.status(201).json(newPerson)
-
-})
-
-app.get("/api/persons/:id", (req, res)=>{
-  const id = Number.parseInt(req.params.id, 10);
-  console.log("🚀 ~ app.get ~ id:", typeof id)
-
-  const person = data.find(person => {
-    return person.id === id
-  })
-  
-  if(person){
-    res.json(person)
-    return
-  }
-  res.status(404).send(`Person with id ${id} not found`)
-})
-
-app.put("/api/persons/:id", (req, res)=>{
-  const personId = Number.parseInt(req.params.id, 10)
-  const {id, name, number} = req.body;
-  const newPerson = {id, name, number}
-const person = data.find(person=>person.id === personId)
-if(!person) return res.status(404).send(`Person with id ${personId} has already been deleted`)
-  data = data.map(person=>person.id ===personId ? newPerson: person)
-res.json(newPerson)
-
-})
-
-app.delete("/api/persons/:id", (req, res)=>{
-  const id = Number.parseInt(req.params.id, 10)
-  const person = data.find(person=>person.id === id)
-  if(!person) return res.status(404).send(`Person with id ${id} not found`)
-    data = data.filter(person=> person.id!==id)
-  res.status(200).send(person)
-})
-
+// Error middleware
+app.use((err, req, res) => {
+   console.log(err.message);
+   if (err.name === "CastError") {
+      return res.status(400).json({ error: "malformatted id" });
+   }
+   res.send(err);
+});
 
 const Port = process.env.PORT || 3001;
 
-app.listen(Port, ()=>{
-    console.log(`App is listening on port ${Port}`)
-})
+app.listen(Port, () => {
+   console.log(`App is listening on port ${Port}`);
+});
